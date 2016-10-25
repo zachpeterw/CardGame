8566392da5be4a3d58202bc2e9c396a6853698ce
zachpeterw <zwhi8516@740s-pl-129.local>
2016-10-25 13:23:02 -0600

    Added card class with data members.

--- CardGame/Model/Card.swift
+++ CardGame/Model/Card.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2016 Whitehead, Zachary. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Card
+{
+    private var backImage : UIImage
+    private var isFaceUp : Bool
+    
+    init()
+    {
+        backImage = UIImage()
+        isFaceUp = false
+    }
+}
