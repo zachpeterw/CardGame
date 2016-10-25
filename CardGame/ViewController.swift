d917fb31d49ddf1a39108975e445ec84f583ea0d
zachpeterw <zwhi8516@740s-pl-129.local>
2016-10-25 13:07:23 -0600

    creating project.

---
+++ CardGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardGame
+//
+//  Created by Whitehead, Zachary on 10/25/16.
+//  Copyright © 2016 Whitehead, Zachary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
