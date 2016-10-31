a6132fcc8b50bcecf850d9e22e8dec9f527a02c1
zachpeterw <zwhi8516@740s-pl-129.local>
2016-10-31 13:00:33 -0600

    added valid rank, max rank, and valid suits methods in playing card.swift

--- CardGame/Model/PlayingCard.swift
+++ CardGame/Model/PlayingCard.swift
@@ -84,6 +84,24 @@ class PlayingCard : Card
         let description = "The card rank is: \(rank) and its suit is \(suit). It has a color of \(color) and \(backStatus)"
         return description
     }
+    
+    //The class modifier makes it so the method is visibile without an instance 
+    //You would call it by ClassName.method()
+    //In this case PlayingCard.validRanks()
+    class func validRanks() -> [String]
+    {
+        return ["??","A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    }
+    
+    class func maxRank() -> Int
+    {
+    return validRanks().count - 1
+    }
+    
+    class func validSuits() -> [String]
+    {
+        return ["♣️","♥️","♦️","♠️"]
+    }
 
 
 }
