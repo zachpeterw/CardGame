a9738392c9ebcd88b8037252cf64e6a61bd03593
zachpeterw <zwhi8516@740s-pl-129.local>
2016-10-31 13:20:41 -0600

    added for in loop to create temp card in playingcarddeck.swift

--- CardGame/Model/PlayingCardDeck.swift
+++ CardGame/Model/PlayingCardDeck.swift
@@ -15,6 +15,17 @@ class PlayingCardDeck : Deck
     override init()
     {
         super.init()
+        
+        for suit in PlayingCard.validSuits()
+        {
+            for var rank = 1; rank < PlayingCard.maxRank(); rank += 1
+            {
+                let tempCard = PlayingCard()
+                tempCard.rank = rank
+                tempCard.suit = suit
+                cards.append(tempCard)
+            }
+        }
     }
     
     func shuffleDeck() -> Void
