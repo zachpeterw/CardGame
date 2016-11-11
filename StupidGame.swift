b30d749a0cfa8c4682e3c47e1af93c278d17fd20
zachpeterw <zwhi8516@740s-pl-129.local>
2016-11-11 13:05:20 -0700

    added Game class

---
+++ StupidGame.swift
@@ -0,0 +1,27 @@
+//
+//  StupidGame.swift
+//  CardGame
+//
+//  Created by Whitehead, Zachary on 11/11/16.
+//  Copyright © 2016 Whitehead, Zachary. All rights reserved.
+//
+
+import Foundation
+
+class StupidGame
+{
+    internal var playDeck : PlayingCardDeck
+    internal var Playerhand : [Card]
+    
+    init()
+    {
+        
+    }
+    
+    func startGame() -> Void
+    {
+    
+        
+    }
+}
+
